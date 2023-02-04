74e7b8b1cad576505bb524f9b091f373c07e4111
Nathan Kolbas <nathan2000@outlook.com>
2023-02-04 13:01:30 -0600

    Finished the structure of the API with an example

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,3 +1,5 @@
+// Original setup from: https://auth0.com/blog/node-js-and-typescript-tutorial-build-a-crud-api/
+
 /**
  * Required External Modules
  */
@@ -5,6 +7,9 @@ import * as dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import { itemsRouter } from "./items/items.router";
+import { errorHandler } from "./middleware/error.middleware";
+import { notFoundHandler } from "./middleware/not-found.middleware";
 
 dotenv.config();
 
@@ -25,6 +30,10 @@ const app = express();
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+app.use("/api/menu/items", itemsRouter);
+
+app.use(errorHandler);
+app.use(notFoundHandler);
 
 /**
  * Server Activation
